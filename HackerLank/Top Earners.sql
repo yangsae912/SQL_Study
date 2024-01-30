b89a08d8261315909e95145519d027430caa14b6
양세연 <104888233+yangsae912@users.noreply.github.com>
2024-01-30 17:45:14 +0900

    Update Top Earners.sql

--- HackerLank/Top Earners.sql
+++ HackerLank/Top Earners.sql
@@ -1,6 +1,19 @@
+-- subquery
 select (months * salary) as earnings, count(distinct employee_id)
 from employee
 where (months * salary) = (
     select max(months * salary) as earnings
     from employee)
 group by 1
+
+-- with 
+
+with query as (
+    select max(months * salary) as earnings
+    from employee
+)
+
+select (months * salary) as earnings, count(distinct employee_id)
+from employee
+where (months * salary) = (select query from max_earnings)
+group by 1;
