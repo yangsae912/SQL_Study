23c936e454722a2d13141846ffa67b4d84f8e12b
Bucket Yang <104888233+yangsae912@users.noreply.github.com>
2024-02-14 14:22:16 +0900

    Create User Activity for the past 30 days I.sql

---
+++ leetcode/User Activity for the past 30 days I.sql
@@ -0,0 +1,15 @@
+select 
+    activity_date as day, 
+    count(distinct user_id) as active_users
+from Activity
+where 
+    activity_date between '2019-06-28' and '2019-07-27'
+group by 1
+
+select 
+    activity_date as day,
+    count(distinct user_id) as active_users
+from activity
+where activity_date> date_sub('2019-07-27', interval 30 day)
+    and activity_date<='2019-07-27'
+group by 1
